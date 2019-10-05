efe30615f565cb30730f3d1f3c5fc10c4e2193ed
Mardoqueu <map@icomp.ufam.edu.br>
2019-10-05 10:42:01 -0400

    new command style parse support

--- include/mardcpp/utils/Arguments.hpp
+++ include/mardcpp/utils/Arguments.hpp
@@ -7,17 +7,18 @@
 #include <mardcpp/std/Pair.hpp>
 #include <mardcpp/std/String.hpp>
 #include <mardcpp/std/UnorderedMap.hpp>
-#include <mardcpp/std/UnorderedSet.hpp>
 #include <mardcpp/utils/util.hpp>
 
 #include <mardcpp/stream/OutputStream.hpp>
+#include <mardcpp/std/Vector.hpp>
 
 namespace mardcpp {
 
 	class Arguments {
+
 	public:
 		using Arg = String;
-		using Args = UnorderedSet<Arg>;
+		using Args = Vector<Arg>;
 		using Kwarg = Pair<Arg, String>;
 		using Kwargs = UnorderedMap<Kwarg::first_type, Kwarg::second_type>;
 
@@ -25,15 +26,20 @@ namespace mardcpp {
 		Args mArgs;
 		Kwargs mKwargs;
 
+		bool mNextIsValue;
+		std::string mNextKey;
+
+		void parseCommands(size_t i, const String &string);
+
 		void parse(const String &string);
 
 	public:
 		Arguments(int argc, const char **argv) noexcept;
 
-		Arguments(std::istream &input) noexcept;
+		explicit Arguments(std::istream &input) noexcept;
 
 		template <typename T>
-		const T getOrDefault(const Arg& arg, const T& defaultValue) const noexcept {
+		T getOrDefault(const Arg& arg, const T& defaultValue) const noexcept {
 			auto it = mKwargs.find(arg);
 			if (it != mKwargs.end()) {
 				return toType<T>(it->second);
@@ -42,18 +48,25 @@ namespace mardcpp {
 		}
 
 		template <typename T>
-		const T get(const Arg& arg) const {
+		T get(const Arg& arg) const {
 			auto it = mKwargs.find(arg);
 			if (it != mKwargs.end()) {
 				return toType<T>(it->second);
 			}
+			throw std::invalid_argument(format<>("Argument \'%s\' not found.", arg.c_str()));
+		}
 
-			static char err[1024];
-			snprintf(err, 1024, "Argument \'%s\' not found!", arg.c_str());
-			throw std::invalid_argument(err);
+		template<typename T>
+		T get(size_t i) {
+			if (i < mArgs.size()) {
+				return toType<T>(mArgs[i]);
+			}
+			throw std::invalid_argument(format<>("Argument %d not found.", i));
 		}
 
 		bool has(const Arg& arg) const noexcept;
+
+		bool has(size_t i) const noexcept;
 
 		inline const Args& getArgs() const noexcept;
 
--- src/utils/Arguments.cpp
+++ src/utils/Arguments.cpp
@@ -5,26 +5,33 @@
 #include <regex>
 
 #include <mardcpp/utils/Arguments.hpp>
-#include <boost/algorithm/string.hpp>
 
 namespace mardcpp {
-	namespace balg = boost::algorithm;
 
-	Arguments::Arguments(int argc, const char **argv) noexcept {
+	Arguments::Arguments(int argc, const char **argv) noexcept
+	: mNextIsValue(false) {
 		for (size_t i = 0; i < static_cast<size_t>(argc); ++i) {
-			parse(argv[i]);
+			parseCommands(i, argv[i]);
+		}
+		if (mNextIsValue) {
+			mArgs.emplace_back(mNextKey);
 		}
 	}
 
-	Arguments::Arguments(std::istream &input) noexcept {
-		String line;
-		while (std::getline(input, line)) {
+	Arguments::Arguments(std::istream &input) noexcept
+	: mNextIsValue(false) {
+		std::string line;
+		for (size_t i = 0; std::getline(input, line); ++i) {
 			parse(line);
 		}
 	}
 
 	bool Arguments::has(const Arguments::Arg &arg) const noexcept {
 		return mKwargs.find(arg) != mKwargs.end();
+	}
+
+	bool Arguments::has(size_t i) const noexcept {
+		return i < mArgs.size();
 	}
 
 	OutputStream &operator<<(mardcpp::OutputStream &os, const mardcpp::Arguments &arguments) {
@@ -38,7 +45,27 @@ namespace mardcpp {
 		if (std::regex_search(string, results, std::regex("^([^= ]+)=([^= ]+)$"))) {
 			mKwargs.emplace(results[1], results[2]);
 		} else {
-			mArgs.emplace(string);
+			mArgs.emplace_back(string);
+		}
+	}
+
+	void Arguments::parseCommands(size_t i, const String &string) {
+		if (mNextIsValue) {
+			mNextIsValue = false;
+			if (string[0] != '-' or string[1] != '-') {
+				mKwargs[mNextKey] = string;
+			} else {
+				mNextIsValue = true;
+				mArgs.emplace_back(mNextKey);
+				mNextKey = string.substr(2);
+			}
+		} else {
+			if (string[0] == '-' and string[1] == '-') {
+				mNextIsValue = true;
+				mNextKey = string.substr(2);
+			} else {
+				mArgs.emplace_back(string);
+			}
 		}
 	}
 
